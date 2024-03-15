6711580e5c53657998093660faa63ca6a42c746e
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-03-15 21:58:28 +0300

    add checkstyle

--- app/src/main/java/hexlet.code/Cli.java
+++ app/src/main/java/hexlet.code/Cli.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class Cli {
-	public static void greeting() {
-		Scanner scanner = new Scanner(System.in);
-		var greeting = new StringJoiner(" ");
-		greeting.add("Hello,");
-		greeting.add(scanner.nextLine());
-		System.out.println(greeting);
-	}
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+        var greeting = new StringJoiner(" ");
+        greeting.add("Hello,");
+        greeting.add(scanner.nextLine());
+        System.out.println(greeting);
+    }
 }
