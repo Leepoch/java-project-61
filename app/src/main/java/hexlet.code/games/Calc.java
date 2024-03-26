ed45c2e214dac9eaf0220ca71edc2e52ae6dc3da
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-03-26 16:35:46 +0300

    update calc & even

--- app/src/main/java/hexlet.code/games/Calc.java
+++ app/src/main/java/hexlet.code/games/Calc.java
@@ -9,30 +9,40 @@ public class Calc {
     private static final int NUMBER_OF_OPERATORS = 3;
     private static final int MAX_NUMBER_VALUE = 21;
 
-    public static void game() {
-        var condition = "What is the result of the expression?";
+    public static int gameData(int firstOperand,int secondOperand,String operator) {
+        var result = 0;
+        switch (operator) {
+            case "+":
+                result = firstOperand + secondOperand;
+                break;
+            case "-":
+                result = firstOperand - secondOperand;
+                break;
+            case "*":
+                result = firstOperand * secondOperand;
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+    public static String[][] prepareData() {
         var numberOfLevels = Engine.NUMBER_OF_LEVELS;
         String[][] gameData = new String[numberOfLevels][2];
+
         for (var i = 0; i < numberOfLevels; i++) {
             var indexOperator = Utils.getRandomNum(NUMBER_OF_OPERATORS);
+            var operators = new String[] {"+", "-", "*"};
             var firstOperand = Utils.getRandomNum(MAX_NUMBER_VALUE);
             var secondOperand = Utils.getRandomNum(MAX_NUMBER_VALUE);
-            switch (indexOperator) {
-                case 0:
-                    gameData[i][1] = Integer.toString(firstOperand + secondOperand);
-                    gameData[i][0] = firstOperand + " + " + secondOperand;
-                    break;
-                case 1:
-                    gameData[i][1] = Integer.toString(firstOperand - secondOperand);
-                    gameData[i][0] = firstOperand + " - " + secondOperand;
-                    break;
-                case 2:
-                    gameData[i][1] = Integer.toString(firstOperand * secondOperand);
-                    gameData[i][0] = firstOperand + " * " + secondOperand;
-                default:
-                    break;
-            }
+            var correctAnswer = gameData(firstOperand, secondOperand, operators[indexOperator]);
+            gameData[i][1] = Integer.toString(correctAnswer);
+            gameData[i][0] = firstOperand + operators[indexOperator] + secondOperand;
         }
-        System.out.println(Engine.source(condition, gameData));
+        return gameData;
+    }
+    public static void game() {
+        var condition = "What is the result of the expression?";
+        System.out.println(Engine.source(condition, prepareData()));
     }
 }
--- app/src/main/java/hexlet.code/games/Even.java
+++ app/src/main/java/hexlet.code/games/Even.java
@@ -6,17 +6,23 @@ import hexlet.code.Utils;
 public class Even {
     public static final int MAX_NUMBER_VALUE = 21;
 
-    public static void game() {
-        var condition = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    public static int gameData() {
+        return Utils.getRandomNum(MAX_NUMBER_VALUE);
+    }
+    public static String[][] prepareData() {
         var numberOfLevels = Engine.NUMBER_OF_LEVELS;
         String[][] gameData = new String[numberOfLevels][2];
 
         for (var i = 0; i < numberOfLevels; i++) {
-            var question = Utils.getRandomNum(MAX_NUMBER_VALUE);
+            var question = gameData();
             String correctAnswer = question % 2 == 0 ? "yes" : "no";
             gameData[i][0] = Integer.toString(question);
             gameData[i][1] = correctAnswer;
         }
-        System.out.println(Engine.source(condition, gameData));
+        return gameData;
+    }
+    public static void game() {
+        var condition = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        System.out.println(Engine.source(condition, prepareData()));
     }
 }
