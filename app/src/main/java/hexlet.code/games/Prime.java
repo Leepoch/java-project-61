f62aaf1bdd36ed6d06fdd077666020e01fcc2ba4
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-03-26 17:18:36 +0300

    update prime

--- app/src/main/java/hexlet.code/games/Prime.java
+++ app/src/main/java/hexlet.code/games/Prime.java
@@ -6,24 +6,14 @@ import hexlet.code.Utils;
 public class Prime {
     public static final int MAX_NUMBER_VALUE = 24;
 
-//    public static int gameData() {
-//
-//    }
-//    public static String[][] prepareData() {
-//
-//
-//        for (var i = 0; i < numberOfLevels; i++) {
-//
-//        }
-//        return gameData;
-//    }
-    public static void game() {
-        var condition = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    public static int gameData() {
+        return Utils.getRandomNum(MAX_NUMBER_VALUE) + 1;
+    }
+    public static String[][] prepareData() {
         var numberOfLevels = Engine.NUMBER_OF_LEVELS;
         String[][] gameData = new String[numberOfLevels][2];
-
+        var primeNumber = gameData();
         for (var i = 0; i < numberOfLevels; i++) {
-            var primeNumber = Utils.getRandomNum(MAX_NUMBER_VALUE) + 1;
             var isPrime = true;
             for (var divisor = 1; divisor <= primeNumber; divisor++) {
                 if (divisor != 1 && divisor != primeNumber && primeNumber % divisor == 0 || primeNumber == 1) {
@@ -33,6 +23,10 @@ public class Prime {
             gameData[i][0] = Integer.toString(primeNumber);
             gameData[i][1] = isPrime ? "yes" : "no";
         }
-        System.out.println(Engine.source(condition, gameData));
+        return gameData;
+    }
+    public static void game() {
+        var condition = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        System.out.println(Engine.source(condition, prepareData()));
     }
 }
