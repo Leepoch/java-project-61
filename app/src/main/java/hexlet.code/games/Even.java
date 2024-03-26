b8ec1a00badfa8c375d6eb221dc11dc3f9a6162d
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-03-26 19:01:01 +0300

    rework even

--- app/src/main/java/hexlet.code/games/Even.java
+++ app/src/main/java/hexlet.code/games/Even.java
@@ -6,16 +6,16 @@ import hexlet.code.Utils;
 public class Even {
     public static final int MAX_NUMBER_VALUE = 21;
 
-    public static int gameData() {
-        return Utils.getRandomNum(MAX_NUMBER_VALUE);
+    public static String gameData(int question) {
+        return question % 2 == 0 ? "yes" : "no";
     }
     public static String[][] prepareData() {
         var numberOfLevels = Engine.NUMBER_OF_LEVELS;
         String[][] gameData = new String[numberOfLevels][2];
 
         for (var i = 0; i < numberOfLevels; i++) {
-            var question = gameData();
-            String correctAnswer = question % 2 == 0 ? "yes" : "no";
+            var question = Utils.getRandomNum(MAX_NUMBER_VALUE);
+            String correctAnswer = gameData(question);
             gameData[i][0] = Integer.toString(question);
             gameData[i][1] = correctAnswer;
         }
