5c86581c94cf1c74a860c6169ffd6de0b8d2c0f6
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-03-26 19:10:02 +0300

    rework prime

--- app/src/main/java/hexlet.code/games/Prime.java
+++ app/src/main/java/hexlet.code/games/Prime.java
@@ -6,20 +6,22 @@ import hexlet.code.Utils;
 public class Prime {
     public static final int MAX_NUMBER_VALUE = 24;
 
-    public static int gameData() {
-        return Utils.getRandomNum(MAX_NUMBER_VALUE) + 1;
+    public static boolean gameData(int primeNumber) {
+        var isPrime = true;
+        for (var divisor = 1; divisor <= primeNumber; divisor++) {
+            if (divisor != 1 && divisor != primeNumber && primeNumber % divisor == 0 || primeNumber == 1) {
+                isPrime = false;
+            }
+        }
+        return isPrime;
     }
     public static String[][] prepareData() {
         var numberOfLevels = Engine.NUMBER_OF_LEVELS;
         String[][] gameData = new String[numberOfLevels][2];
-        var primeNumber = gameData();
+
         for (var i = 0; i < numberOfLevels; i++) {
-            var isPrime = true;
-            for (var divisor = 1; divisor <= primeNumber; divisor++) {
-                if (divisor != 1 && divisor != primeNumber && primeNumber % divisor == 0 || primeNumber == 1) {
-                    isPrime = false;
-                }
-            }
+            var primeNumber = Utils.getRandomNum(MAX_NUMBER_VALUE) + 1;
+            var isPrime = gameData(primeNumber);
             gameData[i][0] = Integer.toString(primeNumber);
             gameData[i][1] = isPrime ? "yes" : "no";
         }
